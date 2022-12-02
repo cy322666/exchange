fdad4834e7148e0de40f90dfa93d47f9a35bae7e
cy322666 <cy322666@gmail.com>
2022-12-02 10:55:31 +0200

    secure_asset add

--- application/resources/views/template-parts/main/header.blade.php
+++ application/resources/views/template-parts/main/header.blade.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
-	<link rel="stylesheet" type="text/css" href={{asset('css/main.css')}}>
+	<link rel="stylesheet" type="text/css" href={{secure_asset(asset('css/main.css'))}}>
 	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-	<script src="{{asset('js/main.js')}}"></script>
+	<script src="{{secure_asset(asset('js/main.js'))}}"></script>
 	<title>Обменка</title>
 </head>
 <body>
